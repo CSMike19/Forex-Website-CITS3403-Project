589e44c990c1a598dd3d4d35ab46f39aa2a386a3
unknown <Vortexflamezz@gmail.com>
2021-07-10 18:18:37 +0800

    yes

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
-login.login_view = 'signin'
 
+def create_app(config_file = 'config.py'):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db = SQLAlchemy(app)
+    migrate = Migrate(app, db)
+    login = LoginManager(app)
+    login.login_view = 'signin'
+    return app
 from app import routes, models, errors
